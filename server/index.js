65de957e064af9893f30b622da9daef13accf2f3
TharunKumar31 <tharunkumarsakthi2004@gmail.com>
2026-06-13 17:06:56 +0530

    Fix railway variable names

--- server/index.js
+++ server/index.js
@@ -11,11 +11,11 @@ connect.use(express.json())
 connect.use(express.static('public'))
 connect.use(bodyparser.urlencoded({ extended: true }))
 let databaseconnection = mysql.createConnection({
-    host: process.env.MYSQL_HOST,
-    port: process.env.MYSQL_PORT,
-    user: process.env.MYSQL_USER,
-    password: process.env.MYSQL_PASSWORD,
-    database: process.env.MYSQL_DATABASE
+    host: process.env.MYSQLHOST,
+    port: process.env.MYSQLPORT,
+    user: process.env.MYSQLUSER,
+    password: process.env.MYSQLPASSWORD,
+    database: process.env.MYSQLDATABASE
 })
 
 databaseconnection.connect(function (error) {
