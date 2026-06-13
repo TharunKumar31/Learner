5f89c69d51d87ac08b6c958eb9e1f89bd315a9a5
TharunKumar31 <tharunkumarsakthi2004@gmail.com>
2026-06-13 16:51:21 +0530

    Fix railway MySQL env names

--- server/index.js
+++ server/index.js
@@ -11,11 +11,11 @@ connect.use(express.json())
 connect.use(express.static('public'))
 connect.use(bodyparser.urlencoded({ extended: true }))
 let databaseconnection = mysql.createConnection({
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME
+    host: process.env.MYSQL_HOST,
+    port: process.env.MYSQL_PORT,
+    user: process.env.MYSQL_USER,
+    password: process.env.MYSQL_PASSWORD,
+    database: process.env.MYSQL_DATABASE
 })
 
 databaseconnection.connect(function (error) {
